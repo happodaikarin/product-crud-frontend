1cfd043d2de08aba7a2bd7b2d22b73152d8d421a
Ramses <rambandan@gmail.com>
2024-10-18 22:49:54 -0500

    new configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://product-crud-backend-iv6c.onrender.com/api', // URL del backend en Render
+  baseURL: window.location.hostname === 'localhost'
+    ? 'http://localhost:8080/api' // URL local si estás en localhost
+    : 'https://product-crud-backend-iv6c.onrender.com/api', // URL en producción
   headers: {
     'Content-Type': 'application/json',
   },
